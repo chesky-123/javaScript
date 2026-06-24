074144ceb0c121850d30bc5c7b3cd02dbd22fefa
chesky <sye0534184736@gmail.com>
2026-06-24 17:46:09 +0300

    add the json files

--- week11/exercises/BankAccountManager/utils.js
+++ week11/exercises/BankAccountManager/utils.js
@@ -1,12 +1,16 @@
-import readlineSync from 'readline-sync';
+import rl from 'readline-sync';
+
+const CUSTOMERS = []
+
+function createCustomer(customerData) {
+  CUSTOMERS.push(customerData);
+}
 
 
 
-
-
-
-const app = readlineSync.question("enter your name: ")
+const app = rl.question("enter your name: ")
 console.log(app)
+const chice = rl.question("enter your choice: ")
 
 
 
