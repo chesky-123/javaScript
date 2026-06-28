688f4c19b50b2520897c68120d8fc028efa25b85
chesky <sye0534184736@gmail.com>
2026-06-28 18:57:58 +0300

    finish project

--- week11/exercises/BankAccountManager/main.js
+++ week11/exercises/BankAccountManager/main.js
@@ -1,15 +1,41 @@
-import manageCustomer from "./service.js" 
+import mng from "./service.js";
+import rl from 'readline-sync';
 
-manageCustomer.createCustomer();
-manageCustomer.createCustomer();
-// console.log(manageCustomer.searchCustomer());
-// console.log(manageCustomer.deposit());
-console.log(manageCustomer.deactivateCustomer());
-// console.log(manageCustomer.deposit());
-console.log(manageCustomer.getAllCustomers());
-console.log(manageCustomer.showStatistics());
-// manageCustomer.withdraw();
-// console.log(manageCustomer.searchCustomer());
+
+
+const actions = [
+    , { "label": "Create Customer", "action": mng.createCustomer },
+    { "label": "Show Customers", "action": mng.getAllCustomers },
+    { "label": "Deposit", "action": mng.deposit },
+    { "label": "Withdraw", "action": mng.withdraw },
+    { "label": "Search Customer", "action": mng.searchCustomer },
+    { "label": "Close Account", "action": mng.deactivateCustomer },
+    { "label": "Show Statistics", "action": mng.showStatistics }
+]
+const menu = `${'='.repeat(50)}
+                Actions Menu
+${'='.repeat(50)}
+`
+
+let selections;
+
+do {
+    console.log(menu);
+    
+    selections = rl.keyInSelect(actions.map(a => a.label), "");
+    if (selections >= 0){
+    console.log(actions[selections].action());
+    };
+} while (selections !== -1);
+    
+
+
+
+
+
+
+
+
 
 
 
